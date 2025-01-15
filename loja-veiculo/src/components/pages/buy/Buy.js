0c8eae605a5ee512f3cbebb457b75e6a0a4c0b28
Marcos Gabriel <marcosindev@gmail.com>
2025-01-15 17:02:04 -0300

    Ajustes

---
+++ loja-veiculo/src/components/pages/buy/Buy.js
@@ -0,0 +1,20 @@
+import styles from './Buy.module.css'
+
+function Buy() {
+
+    return (
+        <section className={styles.container}>
+            <div className={styles.content}>
+                <h1>Compras de carro</h1>
+
+                <div>
+                    <ul>
+                        
+                    </ul>
+                </div>
+            </div>
+        </section>
+    )
+}
+
+export default Buy
--- loja-veiculo/src/index.js
+++ loja-veiculo/src/index.js
@@ -1,11 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Navbar from "./components/layout/Navbar";
+
+import Home from './components/pages/home/Home'
+import Buy from './components/pages/buy/Buy'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <Navbar />
+
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/buy' element={<Buy />} />
+        <Route path='/sell' element={<Home />} />
+        <Route path='/services' element={<Home />} />
+        <Route path='/help' element={<Home />} />
+      </Routes>
+    </Router>
   </React.StrictMode>
 );
