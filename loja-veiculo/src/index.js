4ffcc10eea5dc270288849ce6f3a507414b2d0a5
Marcos Gabriel <marcosindev@gmail.com>
2025-06-10 17:24:38 -0300

    adjustment basename router

--- loja-veiculo/src/index.js
+++ loja-veiculo/src/index.js
@@ -16,7 +16,7 @@ import Anunciar from './components/form/Anunciar.js'
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Router>
+    <Router basename='/loja-veiculos'>
       <Navbar />
 
       <Routes>
